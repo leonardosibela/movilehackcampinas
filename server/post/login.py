f618db38003bb100cd4f6f46f08cd97e8f43c2ef
Henrique Jensen <henriquejensen@hotmail.com>
2019-02-17 07:51:00 -0300

    adds more bills

--- server/post/login.py
+++ server/post/login.py
@@ -1,7 +1,14 @@
 import requests
 import json
+import sys
 from flask import jsonify
 from keys import *
+
+with open("./data/bills.json", "r") as read_file:
+    bills = json.load(read_file)
+
+with open("./data/bills_user.json", "r") as read_file:
+    bills_user = json.load(read_file)
 
 url = baseUrl+'/v1/marketplaces/'+marketPlaceId+'/buyers'
 
@@ -10,34 +17,6 @@ headers = {
     'Authorization': autorizationKey,
     'cache-control': "no-cache",
 }
-
-bills = [
-    {
-        'groupId': "1237468",
-        'fisrt_name': "Aluguel",
-        "created_at": "2019-02-17T07:24:07+00:00",
-        'proratedvalues': [
-            {
-                'id': "97a5205a62ae4d2f9bd13e19a0349b7b",
-                "user": {
-                    "id": "97a5205a62ae4d2f9bd13e19a0349b7b",
-                    "status": "active",
-                    "resource": "buyer",
-                    "account_balance": "0.00",
-                    "current_balance": "0.00",
-                    "first_name": "Joaquim",
-                    "last_name": "Mariano",
-                    "taxpayer_id": "80811217060",
-                    "description": "Morador Rep Campinas",
-                    "email": "joaquimamariano@gmail.com"
-                },
-                "value": 500,
-                "paid": False
-            }
-        ]
-    }
-]
-
 
 def login(payload):
     return requests.request(
@@ -49,4 +28,4 @@ def get_user_by_id(id):
     if (id == '5d1b6cd746c348fbabb46ad728bafabf'):
         return bills
     
-    return []
+    return bills_user
