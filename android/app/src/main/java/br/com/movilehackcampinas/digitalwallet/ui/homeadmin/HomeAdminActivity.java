f21c6394046d4677a9de9b4159fb7c8eda5c2b4f
leonardosibela <leonardo_sibela@hotmail.com>
2019-02-17 05:16:28 -0300

    Adding layout for HomeAdminActivity

---
+++ android/app/src/main/java/br/com/movilehackcampinas/digitalwallet/ui/homeadmin/HomeAdminActivity.java
@@ -0,0 +1,13 @@
+package br.com.movilehackcampinas.digitalwallet.ui.homeadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_admin);
+    }
+}
